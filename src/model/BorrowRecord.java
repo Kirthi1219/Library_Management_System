7baebb99da31b2489fb0d7d162068f2c899b80a1
Madhugayathri Hariharan <HMadhugayathri@corp.mastechinfotrellis.com>
2025-07-18 11:18:05 +0530

    Borrowed record done

---
+++ src/model/BorrowRecord.java
@@ -0,0 +1,118 @@
+package model;
+
+import java.time.LocalDate;
+
+public class BorrowRecord {
+    private int recordId;
+    private int userId;
+    private int bookId;
+    private LocalDate borrowDate;
+    private LocalDate dueDate;
+    private LocalDate returnDate;
+    private boolean renewed;
+    private double fineIncurred;
+    private String returnStatus; 
+
+    public BorrowRecord() {}
+
+    public BorrowRecord(int recordId, int userId, int bookId, LocalDate borrowDate, LocalDate dueDate,
+                        LocalDate returnDate, boolean renewed, double fineIncurred) {
+        this.recordId = recordId;
+        this.userId = userId;
+        this.bookId = bookId;
+        this.borrowDate = borrowDate;
+        this.dueDate = dueDate;
+        this.returnDate = returnDate;
+        this.renewed = renewed;
+        this.fineIncurred = fineIncurred;
+        this.returnStatus = (returnDate == null) ? "Not Returned" : "Returned"; 
+    }
+
+    // Getters & Setters
+    public int getRecordId() {
+        return recordId;
+    }
+
+    public void setRecordId(int recordId) {
+        this.recordId = recordId;
+    }
+
+    public int getUserId() {
+        return userId;
+    }
+
+    public void setUserId(int userId) {
+        this.userId = userId;
+    }
+
+    public int getBookId() {
+        return bookId;
+    }
+
+    public void setBookId(int bookId) {
+        this.bookId = bookId;
+    }
+
+    public LocalDate getBorrowDate() {
+        return borrowDate;
+    }
+
+    public void setBorrowDate(LocalDate borrowDate) {
+        this.borrowDate = borrowDate;
+    }
+
+    public LocalDate getDueDate() {
+        return dueDate;
+    }
+
+    public void setDueDate(LocalDate dueDate) {
+        this.dueDate=dueDate;
+    }
+
+    public LocalDate getReturnDate() {
+        return returnDate;
+    }
+
+    public void setReturnDate(LocalDate returnDate) {
+        this.returnDate = returnDate;
+        this.returnStatus=(returnDate == null) ? "Not Returned" : "Returned"; 
+    }
+
+    public boolean isRenewed() {
+        return renewed;
+    }
+
+    public void setRenewed(boolean renewed) {
+        this.renewed = renewed;
+    }
+
+    public double getFineIncurred() {
+        return fineIncurred;
+    }
+
+    public void setFineIncurred(double fineIncurred) {
+        this.fineIncurred=fineIncurred;
+    }
+
+    public String getReturnStatus() {
+        return returnStatus;
+    }
+
+    public void setReturnStatus(String returnStatus) {
+        this.returnStatus=returnStatus;
+    }
+
+    @Override
+    public String toString() {
+        return "Borrow Record Details:\n" +
+               "Record ID       : " + recordId + "\n" +
+               "User ID         : " + userId + "\n" +
+               "Book ID         : " + bookId + "\n" +
+               "Borrow Date     : " + borrowDate + "\n" +
+               "Due Date        : " + dueDate + "\n" +
+               "Return Date     : " + returnDate + "\n" +
+               "Return Status   : " + returnStatus + "\n" +
+               "Renewed         : " + renewed + "\n" +
+               "Fine Incurred   : ₹" + fineIncurred;
+    }
+}
